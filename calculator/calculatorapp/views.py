87897549e90c6ae09bbb487dba30959d43413207
iambackend <iambackend@gmail.com>
2024-09-10 13:39:13 +0200

    use one page for home and result

--- calculator/calculatorapp/views.py
+++ calculator/calculatorapp/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
+import traceback
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
-
-def result(request):
     formula = request.GET.get('formula')
+    if formula is None:
+        return render(request, 'home.html', {})
 
     # run formula
-    ans = eval(formula)
+    trace = None
+    try:
+        ans = eval(formula)
+    except Exception as e:
+        ans = 'Error'
+        trace = traceback.format_exc()
 
-    return render(request, 'result.html', {'ans': ans})
+    return render(request, 'home.html', {'ans': ans, 'formula': formula, 'trace': trace or None})
