e1a903fb04a2f8be30275acdcf80fbb3b0c01e74
iambackend <iambackend@gmail.com>
2024-09-10 13:16:13 +0200

    working calculator, ignore cache

--- calculator/calculatorapp/views.py
+++ calculator/calculatorapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def result(request):
+    formula = request.GET.get('formula')
+
+    # run formula
+    ans = eval(formula)
+
+    return render(request, 'result.html', {'ans': ans})
